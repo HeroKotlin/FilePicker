b8209098768a16804c02773c44c48ef2d9b9e5a7
musicode <musicode@qq.com>
2019-03-04 22:48:35 +0800

    update

--- library/src/main/java/com/github/herokotlin/filepicker/view/TopBar.kt
+++ library/src/main/java/com/github/herokotlin/filepicker/view/TopBar.kt
@@ -4,9 +4,43 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.RelativeLayout
+import com.github.herokotlin.filepicker.FilePickerConfiguration
 import com.github.herokotlin.filepicker.R
+import kotlinx.android.synthetic.main.file_picker_top_bar.view.*
 
-internal class TopBar: RelativeLayout {
+class TopBar: RelativeLayout {
+
+    lateinit var configuration: FilePickerConfiguration
+
+    var selectedCount = -1
+
+        set(value) {
+
+            if (field == value) {
+                return
+            }
+
+            field = value
+
+            var title = submitButtonTitle
+            if (value > 0) {
+                submitButton.isEnabled = true
+                submitButton.alpha = 1f
+                if (configuration.maxSelectCount > 1) {
+                    title = "$submitButtonTitle($selectedCount/${configuration.maxSelectCount})"
+                }
+            }
+            else {
+                submitButton.isEnabled = false
+                submitButton.alpha = 0.5f
+            }
+
+            submitButton.text = title
+        }
+
+    private val submitButtonTitle: String by lazy {
+        resources.getString(R.string.file_picker_submit_button_title)
+    }
 
     constructor(context: Context) : super(context) {
         init()
